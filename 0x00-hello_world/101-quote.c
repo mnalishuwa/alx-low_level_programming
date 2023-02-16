7057ac2ff106194505d09be7b9492d5977c057e1
mnalishuwa <mnalishuwa@gmail.com>
2023-02-16 21:05:27 +0000

    Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDERR_FILENO, msg, sizeof(msg));
 return (1);
 }
