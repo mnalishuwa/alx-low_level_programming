aad5abc085f2746e1512f461ab210a83812d6132
mnalishuwa <mnalishuwa@gmail.com>
2023-02-16 19:49:57 +0000

    Update puts.c to comply with betty checker

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - program entry point
+ * Description: Start of the program, prints quote thn exits
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
