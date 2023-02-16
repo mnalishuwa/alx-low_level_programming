01172ca0341b86c9301f52f4803ea28885099236
mnalishuwa <mnalishuwa@gmail.com>
2023-02-16 19:58:59 +0000

    Update printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - program start
+ * Description: print a quote and exit
+ * Return: 0 (sucess)
+ */
+
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
