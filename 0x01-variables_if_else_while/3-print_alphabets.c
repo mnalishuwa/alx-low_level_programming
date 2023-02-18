7a1e6da542e6240da4dbe73958e32266a7150e3d
mnalishuwa <mnalishuwa@gmail.com>
2023-02-18 13:49:17 +0000

    Update print alphabets to betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main: program start
+ * main - program start
  * Description: Print alphabet using putchar()
  * Return: 0 (Success)
  */
 
-int main(void)
+int main (void)
 {
         char alphabet[] =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
         int i;
 
-        for (i = 0; alphabet[i] != '\0'; i++) {
+        for (i = 0; alphabet[i] != '\0'; i++)
                 putchar(alphabet[i]);
-        }
+
 
         return (0);
 }
