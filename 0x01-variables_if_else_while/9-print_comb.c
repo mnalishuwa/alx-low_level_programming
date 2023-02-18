ee5c12671a28c76cc51739d6c3297d2b6291ff2b
mnalishuwa <mnalishuwa@gmail.com>
2023-02-18 00:54:07 +0000

    Update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main: program start
+ * Description: Print single digit numbers using putchar function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+  int i;
+
+  for (i = 48; i < 58; ++i)
+  {
+    putchar(i);
+    if (i != 57)
+    {
+      putchar(44);
+      putchar(32);
+    }
+  }
+  putchar(10);
+  return (0);
+}
