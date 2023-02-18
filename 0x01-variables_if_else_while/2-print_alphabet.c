a80d8bd65e3aba2f4c5b0a9e929fa394fa841fe7
mnalishuwa <mnalishuwa@gmail.com>
2023-02-18 01:40:44 +0000

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program start
+ * Description: Print alphabet using putchar()
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+int i;
+
+for (i = 0; alphabet[i] != '\0'; i++)
+{
+putchar(alphabet[i]);
+}
+return (0);
+}
