78bb9d629f9a9cbf327cbdd0a46b20307263668e
mnalishuwa <mnalishuwa@gmail.com>
2023-02-20 10:01:01 +0000

    Update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program start
+ * Description: print all combinations of double digit numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (k = i + 1; k < 58; k++)
+		{
+			putchar(i);
+			putchar(k);
+			putchar(44);
+			putchar(32);
+		}
+	}
+	return (0);
+}
