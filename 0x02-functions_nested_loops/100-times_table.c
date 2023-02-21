e32789203260311d1ee4aa9f13cb466ea457aa22
mnalishuwa <mnalishuwa@gmail.com>
2023-02-21 22:32:42 +0000

    Update print n by n function

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_times_table - print n by n multiplication table
+ * Description: function takes an int n, prints multiplication table n by n
+ *
+ * @n: int representing size of multiplication table to be printed
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n > 15 || n < 0)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+			if (j == 0)
+				printf("%d,", result);
+			else if (j == n)
+				printf("%4d\n", result);
+			else
+				printf("%4d," result);
+		}
+	}
+}
