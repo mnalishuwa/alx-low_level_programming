5b431415842f4fcec2cc98702be214be30580cf2
mnalishuwa <mnalishuwa@gmail.com>
2023-02-23 10:26:29 +0000

    Update main.c for betty compliance

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,31 +1,31 @@
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
+
 /* more headers goes there */
 #include <stdio.h>
+
 /**
  * main - program start
  * Description: print whether a number is positive or negative
  *
  * Return: 0 (Success)
  */
-int main(void)
-{
-int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n < 0)
+void positive_or_negative(int i)
 {
-printf("%d is negative\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is positive\n", n);
-}
-return (0);
+	
+	/* your code goes there */
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
 }
