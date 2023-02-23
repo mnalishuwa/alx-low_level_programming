f119a946057fa62746e847eb77594b583511cb25
mnalishuwa <mnalishuwa@gmail.com>
2023-02-23 09:51:10 +0000

    Update project files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test positive or negative print function
+ * Description: Debugging tests for void positive_or_negative(int i) function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	
+	positive_or_negative(i);
+
+	return (0);
+}
