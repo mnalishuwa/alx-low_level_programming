942902afcf3eec086ce444d8c83e160c22f5023c
mnalishuwa <mnalishuwa@gmail.com>
2023-03-16 16:22:12 +0000

    Correct typo preventing compilation

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,12 +21,12 @@ int *array_range(int min, int max)
 	_array = malloc((1 + max - min) * sizeof(int));
 
 	/* check if the min is less than max or if malloc succeeds */
-	if ((min > max) < 0 || !_array)
+	if ((min > max) || !_array)
 		return (NULL);
 
 	/* loop through and set values from min to max */
 	for (i = 0; i <= (max - min); i++)
 		_array[i] = min + i;
 
-	return _array;
+	return (_array);
 }
