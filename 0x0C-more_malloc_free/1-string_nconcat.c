c196986446d1cf8314cdcbc6dfd05ebb9d028af1
mnalishuwa <mnalishuwa@gmail.com>
2023-03-16 15:48:31 +0000

    Correct typos preventing compilation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,14 +16,14 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *nconcat_s;
-	int total_len, len_s1, len_s2, i;
+	unsigned int total_len, len_s1, len_s2, i;
 
 	if (!s1 && !s2)
-		return string_nconcat("", "");
+		return (string_nconcat("", "", n));
 	if (!s1 && s2)
-		return string_nconcat("", s2);
+		return (string_nconcat("", s2, n));
 	if (s1 && !s2)
-		return string_nconcat(s1, "");
+		return (string_nconcat(s1, "", n));
 
 	len_s1 = strlen(s1);
 	len_s2 = strlen(s2);
@@ -33,16 +33,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		total_len = len_s1 + len_s2;
 
 	nconcat_s = malloc(total_len * sizeof(char));
-	if (!nconcat)
+	if (!nconcat_s)
 	{
-		free(nconcat);
+		free(nconcat_s);
 		return (NULL);
 	}
 
 	for (i = 0; i < len_s1; i++)
-		nconcat[i] = s1[i];
+		nconcat_s[i] = s1[i];
 	for (i = 0; (len_s1 + i) < total_len; i++)
-		nconcat[len_s1 + i] = s2[i];
+		nconcat_s[len_s1 + i] = s2[i];
 
-	return (nconcat);
+	return (nconcat_s);
 }
