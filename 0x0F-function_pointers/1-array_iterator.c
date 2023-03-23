852ee0e434234b65f4482caf90500abf101ec478
mnalishuwa <mnalishuwa@gmail.com>
2023-03-23 08:28:47 +0000

    Update for betty compliance, add null checks to prevent segfaults

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator: applies a function to each element of an array
+ * array_iterator - applies a function to each element of an array
  * Description: takes a pointer to an int array, an unsigned long
  * indicating the number of elements in the array, and a pointer
  * to a function that takes an integer argument and returns void.
@@ -15,8 +15,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0; /* decrement size to start from last item in array */
-	/* apply action function to each element starting from the last */
+	size_t i = 0;
+
+	/* check if array or action is NULL and return */
+	if (!array || !action)
+		return;
+	/* apply action function to each element */
 	for (; i < size; action(array[i]), i++)
 		;
 }
