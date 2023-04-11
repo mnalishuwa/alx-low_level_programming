271e602560d58a4e26d4b530d780e2a4416cccae
mnalishuwa <mnalishuwa@gmail.com>
2023-04-11 07:55:30 +0000

    Return error on index > max_bits for get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,10 +13,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bmask;
 
-	bmask = _pow_recursion(2, index);
-
-	if (bmask > n)
+	if (index > 64)
 		return (-1);
+
+	bmask = _pow_recursion(2, index);
 
 	if (n & bmask)
 		return (1);
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -20,3 +20,4 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		*n = *n ^ bmask;
 	return (1);
 }
+
