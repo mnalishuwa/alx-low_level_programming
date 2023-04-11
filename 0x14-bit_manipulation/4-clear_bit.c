aa76481cac0f403e939caed1cdea2c8d8b28338f
mnalishuwa <mnalishuwa@gmail.com>
2023-04-11 10:58:12 +0000

    Update clear_bit for index > max_bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,13 +11,55 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bmask = 1 << index;
+	unsigned long int bmask;
 
-	if (n == NULL)
+	if (n == NULL || index > 64)
 		return (-1);
 
-	if (bmask <= *n)
+	bmask = 1 << index;
+
+	if (get_bit(*n, index))
 		*n = *n ^ bmask;
 	return (1);
 }
 
+/**
+ * get_bit - returns the value of a bit
+ * Description: returns the bit value given an index
+ *
+ * @n: number with desired bits
+ * @index: index of the desired bit
+ *
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bmask;
+
+	if (index > 64)
+		return (-1);
+
+	bmask = _pow_recursion(2, index);
+
+	if (n & bmask)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * _pow_recursion - return x ^ y
+ * Description: computes x ^ y recursively
+ *
+ * @x: int arg 1
+ * @y: int arg 2
+ *
+ * Return: int x ^ y
+ */
+unsigned int _pow_recursion(unsigned int x, unsigned int y)
+{
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
