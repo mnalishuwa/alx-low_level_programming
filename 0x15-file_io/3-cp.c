18df2e1fb6c80b4617d558cb7cd5ba8997678a52
mnalishuwa <mnalishuwa@gmail.com>
2023-04-14 23:49:52 +0000

    Add copy file program

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,90 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void _chk_close(int fd, int _nclose);
+
+/**
+ * main - program copies from source to dest
+ * Description: program copies content of one file to a new file
+ *
+ * @argc: int, argument count
+ * @argv: pointer to array of pointers to arguments, argument vector
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	ssize_t n_read, n_write;
+	size_t buffer_size = 1024;
+	int file_desc_src, file_desc_dest, n_close;
+	char _buffer[1024];
+
+	if (argc - 1 != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_desc_src = open(argv[1], O_RDONLY);
+	if (file_desc_src < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		n_close = close(file_desc_src);
+		_chk_close(file_desc_src, n_close);
+		exit(98);
+	}
+
+	file_desc_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_desc_dest < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		n_close = close(file_desc_dest);
+		_chk_close(file_desc_dest, n_close);
+		exit(99);
+	}
+
+	do {
+		n_read = read(file_desc_src, &_buffer, buffer_size);
+		if (n_read < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			n_close = close(file_desc_src);
+			_chk_close(file_desc_src, n_close);
+			exit(98);
+		}
+
+		n_write = write(file_desc_dest, &_buffer, (size_t)n_read);
+		if (n_write < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			n_close = close(file_desc_dest);
+			_chk_close(file_desc_dest, n_close);
+			exit(99);
+		}
+	} while (n_read > 0);
+
+	return (0);
+}
+
+/**
+ * _chk_close - attempt to close file
+ * Description: close file
+ *
+ * @fd: int, file descriptor
+ * @_nclose: int, close value
+ *
+ * Return: void
+ */
+void _chk_close(int fd, int _nclose)
+{
+	if (_nclose < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
