8322cf74fd799e94cb3ab207e814c70aa7c07ba9
mnalishuwa <mnalishuwa@gmail.com>
2023-04-26 09:31:30 +0000

    Update realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -41,7 +41,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (p)
 	{
-		for (i = 0; i < size && i < new_size; i++)
+		for (i = 0; i < old_size && i < new_size; i++)
 			*((unsigned char *)p + i) = *((unsigned char *)ptr + i);
 	}
 
