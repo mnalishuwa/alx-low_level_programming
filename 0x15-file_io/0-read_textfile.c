5739beefc4a237c76b6fb75038217758c3efc7b0
mnalishuwa <mnalishuwa@gmail.com>
2023-07-10 20:15:39 +0000

    Add solution files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints contents to stdout
+ * Description: Receives a text file and writes the contexts to
+ * standard output using the stdout file descriptor, STDOUT_FILENO
+ *
+ * @filename: string, name of the text file
+ * @letters: number of chars
+ *
+ * Return: size_t, number of bytes written
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t bytes_read, bytes_written;
+	int _textfile_desc;
+	char *text_buf = malloc(letters * sizeof(char));
+
+	_textfile_desc = open(filename, O_RDONLY); /* Open file, readonly mode */
+	if (filename == NULL || (_textfile_desc < 0) || text_buf == NULL)
+	{ /* check if file name, check if the file was successfully opened */
+		close(_textfile_desc);
+		free(text_buf);
+		return (0);
+	}
+	/* read through specified number of chars into text_buf*/
+	bytes_read = read(_textfile_desc, text_buf, letters);
+	if (bytes_read == -1)
+	{ /* close file and return if read unsuccessful */
+		close(_textfile_desc);
+		free(text_buf);
+		return (0);
+	}
+	/* Write specified number of chars from text_buf to stdout*/
+	bytes_written = write(STDOUT_FILENO, text_buf, letters);
+	if (bytes_written == -1)
+	{ /* close file and return if write unsuccessful */
+		close(_textfile_desc);
+		free(text_buf);
+		return (0);
+	}
+	close(_textfile_desc); /* close file after read */
+
+	free(text_buf);
+
+	return (bytes_written); /* return number of chars read */
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,49 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <elf.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/* _putchar - write a single char to stdout */
+int _putchar(char c);
+
+/* read_textfile - reads a text file and prints it to stdout */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* int create_file - function creates a new file */
+int create_file(const char *filename, char *text_content);
+
+/* append_text_to_file - appends text to the end of a file */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* print_magic - prints the magic number */
+void print_magic(void *ebuf);
+
+/* checks if ELF is valid based on the 1st 4 bytes of header */
+int is_elf(void *ebuf);
+
+void elf_endian(unsigned char *ebuf);
+
+void print_version(unsigned char *ebuf);
+
+void elf_class(unsigned char *ebuf);
+
+void print_abi_version(unsigned char *ebuf);
+
+char *check_osabi(unsigned char *ebuf);
+
+char *check_type(void *ebuf);
+
+void print_entry(char *fname, unsigned char *ebuf);
+
+void _print_eentry(unsigned char *ebuf);
+
+int get_endianness(void);
+
+#endif
