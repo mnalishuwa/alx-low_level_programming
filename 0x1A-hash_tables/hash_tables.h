c69600b217ce6e50f46b7d6d082fcef3e38f6481
mnalishuwa <mnalishuwa@gmail.com>
2023-09-13 20:08:26 +0000

    Add djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,5 +36,7 @@ typedef struct hash_table_s
 
 /* Create hash table */
 hash_table_t *hash_table_create(unsigned long int size);
+/* Djb2 Hash Function */
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
