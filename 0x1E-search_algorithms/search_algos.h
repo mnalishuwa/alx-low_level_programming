a1b01784a6764ddaae9c6ff44589646d838599de
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2024-02-07 23:50:23 +0000

    Add binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,7 @@
 /* searches for a value in an array of integers using the Linear search */
 int linear_search(int *array, size_t size, int value);
 
+/* searches for a value in array using binary search */
+int binary_search(int *array, size_t size, int value);
+
 #endif
