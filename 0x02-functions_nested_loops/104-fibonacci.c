5c6bb1ed474cca3fccbe4eb2418ee313067c2e55
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2024-06-19 23:28:42 +0000

    Update fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,84 @@
+#include <stddef.h>
 #include <stdio.h>
-#include <limits.h>
-#define FIB1 1
-#define FIB2 2
-#define BASE_LMAX LONG_MAX
+
+void add_printfib(char *fib1, char *fib2, char *result,
+		  size_t size_result, size_t size_num);
 /**
  * main - program start
  * Description: program to prints first 50 fibonacci numbers
  * Return: 0 (success)
  */
 
-/* Try long base solution and __int128 solutions */
 int main(void)
 {
-	int size = 98;
-	long int fib_arr[size] = {0}; /* fib number array in base LMAX */
-	long int fib_arr_dec[size * 2] = {0};
-	int i, j, slots_per_fib;
+	char fib1[25] = {'$'};
+	char fib2[25] = {'$'};
+	char result[26] = {'$'};
+	size_t i;
+	size_t size_result = sizeof(result) / sizeof(result[0]);
+	size_t size_num = sizeof(fib1) / sizeof(fib1[0]);
 
-	/* set 1st and 2nd array element to hold initial fibonacci numbers */
-	fib_arr[0] = FIB1;
-	fib_arr[1] = FIB2;
-
-	printf("%d, %d, ", FIB1, FIB2);
-	/* loop through and generate successive fib numbers */
-	for (i = FIB2; i < 98; i++)
+	for (i = 0; i < size_result; i++)
 	{
-		if (LONG_MAX - fib_arr[i -1] < fib_arr[i - 2)
-		{				
-
-		}
-		else
+		result[i] = '$';
+		if (i < size_num)
 		{
-				fib_arr[i] = fib_arr[i - 1] + fib_arr[i - 2];
-				if (i == 97)
-					printf("%lu\n", fib_arr[i]);
-				else
-					printf("%lu, ", fib_arr[i]);
+			fib1[i] = '$';
+			fib2[i] = '$';
 		}
 	}
+	fib1[size_num - 1] = '1' - '0';
+	fib2[size_num - 1] = '2' - '0';
+	printf("%hu, %hu, ", fib1[size_num - 1], fib2[size_num - 1]);
+
+	add_printfib(fib1, fib2, result, size_result, size_num);
 	return (0);
+}
+
+/**
+ * add_printfib - add fib numbers
+ * Description: take two fib numbers and add together and store in result
+ *
+ * @fib1: fibn - 2 represented as char array
+ * @fib2: fibn - 1 represented as char array
+ * @result: sum
+ * @size_result: number of digits result
+ * @size_num: number of digits fib numbers
+ *
+ * Return: void
+ */
+
+void add_printfib(char *fib1, char *fib2, char *result,
+		  size_t size_result, size_t size_num)
+{
+	char carry = '0' - '0', sum;
+	int i, j, k;
+
+	k = size_num - 1;
+	for (j = 0; j < 95; j++)
+	{
+		for (i = (int)size_num - 1, carry = 0; i >= 0 && fib2[i] != '$'; i--)
+		{
+			if (fib1[i] == '$')
+				fib1[i] = 0;
+			sum = (fib1[i] + fib2[i] + carry) % 10;
+			carry = (fib1[i] + fib2[i] + carry) / 10;
+			result[i + 1] = sum;
+			fib1[i] = fib2[i];
+			fib2[i] = sum;
+		}
+		if (carry > 0)
+		{
+			result[i + 1] = carry;
+			fib2[i] = carry;
+		}
+		for (i++, k = 0; i < (int)size_result; i++)
+		{
+			if (result[i] == '$')
+				continue;
+			printf("%hu", result[i]);
+			k++;
+		}
+		(j != 94) ? printf(", ") : printf("\n");
+	}
 }
